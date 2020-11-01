fa1c9bf125f1c725f4665cc379ce7543db56652f
Carlos Brand <carlosbrand@Carloss-iMac.local>
2020-10-31 22:42:17 -0700

    first stage

---
+++ script.js
@@ -0,0 +1,23 @@
+//PSEUDOCODE
+//*setInterval to create the timer
+//clearInterval to clear the timout
+// VAR currentScore, timeLeft
+// VAR questions -Array
+// VAR pointer/index-Current position in the question array
+
+//-------VAR questions = [ 
+    //{
+        //question text
+        //list of question answers
+        //correct Answer  
+//  //}
+
+//      ] 
+
+var questions = [
+    
+    {
+    Q1:"In Street fighter II, what is the nationality of the character named Ryu?",
+
+    }
+];
